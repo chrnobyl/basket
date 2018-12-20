15fd70a6209830c1c583f9418b26e388987fbee4
github username <cjab86@gmail.com>
2018-12-20 10:17:47 -0500

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-
 module.exports = {
     devtool: 'source-map',
     entry: "./src/index.js",
